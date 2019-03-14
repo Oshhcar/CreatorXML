5111adcfb0c3e79021e740cd943c678fe48e71ad
Oscar M <oskralberto14@gmail.com>
2019-03-14 15:20:12 -0600

    Agregando unario para aumento y decremento en una instruccion

---
+++ src/fs/ast/instruccion/Unario.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fs.ast.instruccion;
+
+import fs.ast.expresion.AccesoArreglo;
+import fs.ast.expresion.AccesoObjeto;
+import fs.ast.expresion.Expresion;
+import fs.ast.expresion.Identificador;
+import fs.ast.expresion.Literal;
+import fs.ast.expresion.operacion.Operacion;
+import fs.ast.simbolos.TablaSimbolo;
+import fs.ast.simbolos.Tipo;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author oscar
+ */
+public class Unario implements Instruccion {
+
+    private final Expresion exp;
+    private final Operacion.Operador operador;
+    private final int linea;
+    private final int columna;
+
+    public Unario(Expresion exp, Operacion.Operador operador, int linea, int columna) {
+        this.exp = exp;
+        this.operador = operador;
+        this.linea = linea;
+        this.columna = columna;
+    }
+
+    @Override
+    public Object ejecutar(TablaSimbolo tabla, JTextArea salida) {
+        Object valExp = exp.getValor(tabla, salida);
+        if (valExp != null) {
+            Tipo tipoExp = exp.getTipo(tabla);
+            if (tipoExp != null) {
+                if (tipoExp.isNumero()) {
+                    Object val;
+                    if (tipoExp == Tipo.ENTERO) {
+                        if (operador == Operacion.Operador.AUMENTO) {
+                            val = new Integer(valExp.toString()) + 1;
+                        } else {
+                            val = new Integer(valExp.toString()) - 1;
+                        }
+                    } else {
+                        if (operador == Operacion.Operador.AUMENTO) {
+                            val = new Double(valExp.toString()) + 1;
+                        } else {
+                            val = new Double(valExp.toString()) - 1;
+                        }
+                    }
+
+                    Literal valor = new Literal(tipoExp, val, linea, columna);
+
+                    if (exp instanceof Identificador) {
+                        Identificador id = (Identificador) exp;
+                        Asignacion asigna = new Asignacion(id.getId(), valor, linea, columna);
+                        return asigna.ejecutar(tabla, salida);
+                    } else if (exp instanceof AccesoArreglo) {
+                        AccesoArreglo ar = (AccesoArreglo) exp;
+                        AsignacionArreglo asigna = new AsignacionArreglo(ar.getId(), ar.getPosicion(), valor, linea, columna);
+                        asigna.ejecutar(tabla, salida);
+                    } else if (exp instanceof AccesoObjeto) {
+                        AccesoObjeto obj = (AccesoObjeto) exp;
+                        AsignacionObjeto asigna = new AsignacionObjeto(obj.getId(), obj.getClave(), obj.getPosicion(), valor, linea, columna);
+                        asigna.ejecutar(tabla, salida);
+                    } else {
+                        if (operador == Operacion.Operador.AUMENTO) {
+                            System.err.println("Error, se necesita de una variable para aumentar. Línea: " + linea);
+                        } else {
+                            System.err.println("Error, se necesita de una variable para decrementar. Línea: " + linea);
+
+                        }
+                    }
+
+                } else {
+                    if (operador == Operacion.Operador.AUMENTO) {
+                        System.err.println("Error de tipos, solo los números se pueden aumentar. Línea: " + linea);
+                    } else {
+                        System.err.println("Error de tipos, solo los números se pueden decrementar. Línea: " + linea);
+                    }
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getLinea() {
+        return linea;
+    }
+
+    @Override
+    public int getColumna() {
+        return columna;
+    }
+
+}
